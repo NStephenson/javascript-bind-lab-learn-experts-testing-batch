f646c1cc7e3c5eb60a74cef3ce5061bfdbfbbda7
NStephenson <nickstephenson90@gmail.com>
2016-12-06 13:24:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function () {
+    setInterval(() => this.time++, 1000);
+  }
+};
+
+function censor(censoredWord, str){
+  var exp = new RegExp(censoredWord, "g")
+  return str.replace(exp, "BLEEP")
+}
+
+const violenceCensor = censor.bind(null, "violence")
+
+const drugsCensor = censor.bind(null, "drugs")
